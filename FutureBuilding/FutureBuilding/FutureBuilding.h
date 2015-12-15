e8a70915a04305499f2b2578c178f35219055a27
qinyinghao <jsqinyinghao@live.com>
2015-12-15 10:14:21 +0800

    FIRST COMMIT

---
+++ FutureBuilding/FutureBuilding/FutureBuilding.h
@@ -0,0 +1,32 @@
+
+// FutureBuilding.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CFutureBuildingApp:
+// 有关此类的实现，请参阅 FutureBuilding.cpp
+//
+
+class CFutureBuildingApp : public CWinApp
+{
+public:
+	CFutureBuildingApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CFutureBuildingApp theApp;
